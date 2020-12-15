2724c6de2380c36aec5fe62a0650eb826b4ca80f
Tomek Abramowicz <tabramowicz@gmail.com>
2020-12-15 12:35:37 +0100

    Update firewall.tf

--- firewall.tf
+++ firewall.tf
@@ -1,12 +1,23 @@
 resource "google_compute_firewall" "allow-https" {
   provider = google-beta
-  name = "website-fw-4"
-  network = google_compute_network.default.id
+  name = "allow-https-rule"
+  network = google_compute_network.frontend-network.id
   source_ranges = ["10.164.0.0/20"]
-  target_tags = ["load-balanced-frontend"]
   allow {
     protocol = "tcp"
     ports = ["443"]
+  }
+  direction = "INGRESS"
+}
+
+resource "google_compute_firewall" "allow-ssh" {
+  provider = google-beta
+  name = "allow-ssh-rule"
+  network = google_compute_network.backend-network.id
+  source_ranges = ["0.0.0.0/0"]
+  allow {
+    protocol = "tcp"
+    ports = ["22"]
   }
   direction = "INGRESS"
 }
