593d30b4666c03a7b6938dd7a11336e2610be675
Tomek Abramowicz <tabramowicz@gmail.com>
2020-12-15 12:32:44 +0100

    Update instances-dbs.tf

--- instances-dbs.tf
+++ instances-dbs.tf
@@ -19,3 +19,49 @@ resource "google_compute_instance" "db-master" {
     subnetwork = "backend-subnetwork"
   }
 }
+
+resource "google_compute_disk" "db-master" {
+    name = "db-master-disk"
+    type = "pd-ssd"
+    zone = "europe-west4-a"
+    size = "10"
+}
+
+resource "google_compute_attached_disk" "default" {
+    disk = "${google_compute_disk.db-master-disk.self_link}"
+    instance = "${google_compute_instance.db-master.self_link}"
+}
+
+resource "google_compute_instance" "db-slave" {
+  count = "2"
+  name = "db-slave-${index.count+1}"
+  machine_type = "n1-standard-1"
+  zone = "europe-west4-a"
+  can_ip_forward = "false"
+
+    tags = []
+
+
+  boot_disk {
+    initialize_parmas {
+        image = "centos-7-v20201112"
+    }
+  }
+
+  network_interface {
+    network = "backend-network"
+    subnetwork = "backend-subnetwork"
+  }
+}
+
+resource "google_compute_disk" "db-slave" {
+    name = "db-slave-disk"
+    type = "pd-ssd"
+    zone = "europe-west4-a"
+    size = "10"
+}
+
+resource "google_compute_attached_disk" "default" {
+    disk = "${google_compute_disk.db-slave-disk.self_link}"
+    instance = "${google_compute_instance.db-slave[0].self_link}"
+}
